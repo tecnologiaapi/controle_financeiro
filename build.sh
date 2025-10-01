870baf2669e495a1e8a369bb3bbfa74e210d6d84
tecnologiaapi <tecnologia.siilva@gmail.com>
2025-10-01 10:38:19 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
-# build.sh
 
-# Sair imediatamente se um comando falhar
+build.sh
+#Sair imediatamente se um comando falhar
 set -o errexit
 
-# Instalar as dependências do Python
+#Instalar as dependências do Python
 pip install -r requirements.txt
 
-# Criar as tabelas do banco de dados
-# (Isso executará o comando db.create_all() no contexto da sua aplicação)
-flask shell <<< "from app import db; db.create_all()"
+#Aplicar as migrações do banco de dados (cria as tabelas)
+flask db upgrade
 
 echo "Build finalizado com sucesso!"
